fa5cb8e1fc52eccec337bb4c353cfec91c4b212e
Ryan Soury <ryan@webdoodle.com.au>
2024-10-17 17:56:18 +1100

    refine the screenshot mechanism

--- src/helper/src/lib.rs
+++ src/helper/src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
-use std::marker::{Send,Sync};
+use std::marker::{ Send, Sync };
 
-use serde::{Deserialize, Serialize,};
+use serde::{ Deserialize, Serialize };
 use tracing::debug;
 use reqwest;
 use std::collections::HashMap;
@@ -9,85 +9,111 @@ use serde_json::json;
 use headless_chrome::Browser;
 use headless_chrome::protocol::cdp::Page;
 
-pub async fn create_twitter_post_image(url:String)->Result<Vec<u8>, Box<dyn Error+Send+Sync>>  {
+pub async fn create_twitter_post_image(
+    url: String
+) -> Result<Vec<u8>, Box<dyn Error + Send + Sync>> {
+    let prefix = ["https://x.com", "https://twitter.com"];
 
+    assert!(
+        url.to_lowercase().starts_with(&prefix[0]) || url.to_lowercase().starts_with(&prefix[1]),
+        "Hostname must be twitter.com or x.com"
+    );
 
-    let prefix = ["https://x.com","https://twitter.com"];
+    let mut builder = headless_chrome::LaunchOptions::default_builder();
+    // Set headless mode based on whether it's in test mode
+    #[cfg(test)]
+    builder.headless(false); // Headful mode for tests
 
-    assert!(url.to_lowercase().starts_with(&prefix[0])||url.to_lowercase().starts_with(&prefix[1]), "Hostname must be twitter.com or x.com");
-    
+    #[cfg(not(test))]
+    builder.headless(true); // Headless mode for non-test
 
-    let browser = Browser::default()?;
+    let browser = Browser::new(builder.build()?)?;
 
-    browser.get_process_id();
     let tab = browser.new_tab()?;
     tab.set_default_timeout(std::time::Duration::from_secs(60));
+    let _ = tab.set_user_agent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        None,
+        None
+    );
 
     // Set screen Dimension (in this case mobile)
-    let tab = tab.set_bounds(
-        headless_chrome::types::Bounds::Normal { 
-            left: Some(0), 
-            top: Some(0), 
-            width: Some(720.0), 
-            height: Some(4500.0) 
-        })?;
+    let tab = tab.set_bounds(headless_chrome::types::Bounds::Normal {
+        left: Some(0),
+        top: Some(0),
+        width: Some(375.0),
+        height: Some(1000.0),
+    })?;
 
     // Navigate to wikipedia
     tab.navigate_to(&url)?;
-    
 
     tab.wait_until_navigated()?;
 
-    let page_error=tab.find_element_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[3]/div/span");
+    let page_error = tab.find_element_by_xpath(
+        "/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[3]/div/span"
+    );
     match page_error {
         Ok(element) => {
-           let data= element.get_inner_text();
-           match data {
-               Ok(data)=>{
-                   return Err(format!("PageError Found: {}", data).into());
-               },
-               Err(_)=>{},
-           }
-        },
+            let data = element.get_inner_text();
+            match data {
+                Ok(data) => {
+                    return Err(format!("PageError Found: {}", data).into());
+                }
+                Err(_) => {}
+            }
+        }
         Err(_) => {
             // Proceed with execution for successful case
             println!("Page didn't error out");
         }
     }
 
-    let view_port = tab.wait_for_elements_by_xpath("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]").unwrap()[0].get_box_model().unwrap().content_viewport();
+    let view_port_selector =
+        "#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010 > main > div > div > div > div > div > section > div > div > div:nth-child(1) > div > div > article > div.css-175oi2r.r-eqz5dr.r-16y2uox.r-1wbh5a2 > div";
+    let view_port_element = tab.wait_for_element(view_port_selector).unwrap();
+    let view_port = view_port_element.get_box_model().unwrap().content_viewport();
+
+    // println!("View Port: {:?}", view_port);
 
     // Take a screenshot a cropped view of the browser window
-    let jpeg_data = tab.capture_screenshot(
+    let image_data = tab.capture_screenshot(
         Page::CaptureScreenshotFormatOption::Png,
         Some(100),
-        Some(Page::Viewport { x: view_port.x, y: view_port.y, width:view_port.width, height: view_port.height-109.0, scale: 2.0 }),
-        true)?;
+        Some(Page::Viewport {
+            x: view_port.x - 10.0,
+            y: view_port.y - 10.0,
+            width: view_port.width + 20.0,
+            height: view_port.height - 146.0 + 20.0,
+            scale: 2.0,
+        }),
+        true
+    )?;
 
-    Ok(jpeg_data)
+    Ok(image_data)
 }
 
-pub async fn create_twitter_post_image_from_id(tweet_id :u64)->Result<Vec<u8>, Box<dyn Error+Sync+Send>>  {
+pub async fn create_twitter_post_image_from_id(
+    tweet_id: u64
+) -> Result<Vec<u8>, Box<dyn Error + Sync + Send>> {
     return create_twitter_post_image(format!("https://x.com/x/status/{}", tweet_id)).await;
 }
 
-
 #[cfg(test)]
-mod tests {    
+mod tests {
     use super::*;
     use tokio;
     use headless_chrome;
 
     #[tokio::test]
-    async fn test_url_starts_with_twitter_com() {
-
+    async fn test_create_image() {
         println!("{:?}", headless_chrome::browser::default_executable());
-        let tweet_id:u64 = 1834071245224308850;
+        let tweet_id: u64 = 1834071245224308850;
         match create_twitter_post_image_from_id(tweet_id).await {
             Ok(bytes) => {
                 assert!(bytes.len() > 0, "Image bytes should not be empty");
-                    // Save the screenshot to disc
-                    let _ = std::fs::write("./screenshot.png", bytes);
+                // Save the screenshot to disc
+                let _ = std::fs::write("./screenshot.png", bytes);
             }
             Err(e) => panic!("Expected Ok, but got Err: {:?}", e),
         }
@@ -108,7 +134,7 @@ mod tests {
         let url = "https://EXAMPLE.COM/some/path".to_string();
         // This should fail as the URL does not start with "https://x.com" or "https://twitter.com".
         let result = std::panic::catch_unwind(|| {
-            let _ =  futures::executor::block_on(create_twitter_post_image(url));
+            let _ = futures::executor::block_on(create_twitter_post_image(url));
         });
         assert!(result.is_err(), "The URL was expected to be invalid");
     }
@@ -118,7 +144,7 @@ mod tests {
         let url = "".to_string();
         // This should fail as the URL is empty and does not start with either prefix.
         let result = std::panic::catch_unwind(|| {
-            let _ =   futures::executor::block_on(create_twitter_post_image(url));
+            let _ = futures::executor::block_on(create_twitter_post_image(url));
         });
         assert!(result.is_err(), "The URL was expected to be invalid");
     }
@@ -132,7 +158,7 @@ struct Data {
     metadata: Metadata,
 }
 
-#[derive(Serialize, Deserialize, Debug , Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Metadata {
     url: Option<String>,
     hash: Option<String>,
@@ -147,45 +173,47 @@ struct Message {
     id: String,
 }
 
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Response {
     messages: Vec<Message>,
 }
 pub struct BitteImageGenerator<'a> {
     pub session_key: String,
-    pub creator: &'a str
+    pub creator: &'a str,
 }
 
 impl<'a> BitteImageGenerator<'a> {
-    pub async fn new(creator: &'a str,) -> Result<Self, Box<dyn Error>> {
-        
+    pub async fn new(creator: &'a str) -> Result<Self, Box<dyn Error>> {
         // Serialize the payload to JSON
-        let payload = json!({
+        let payload =
+            json!({
             "creator": &creator,
             "message": "I need an NFT minted for a tweet",
         });
 
         let client = reqwest::Client::new();
-        let response = client.post(format!("{}api/smart-action/create", API_URL_BASE))
+        let response = client
+            .post(format!("{}api/smart-action/create", API_URL_BASE))
             .json(&payload)
-            .send()
-            .await.unwrap().json::<HashMap<String, String>>().await.unwrap();
-        
-            println!("{:?}", response);
-            let session_key = response.get("id").ok_or("Missing 'id' in response")?;
-            let session_key = session_key.clone();
-            debug!("{:?}", response);
+            .send().await
+            .unwrap()
+            .json::<HashMap<String, String>>().await
+            .unwrap();
 
+        println!("{:?}", response);
+        let session_key = response.get("id").ok_or("Missing 'id' in response")?;
+        let session_key = session_key.clone();
+        debug!("{:?}", response);
 
-            Ok(BitteImageGenerator { 
-                session_key: session_key.clone(),
-                creator: creator
-            })
+        Ok(BitteImageGenerator {
+            session_key: session_key.clone(),
+            creator: creator,
+        })
     }
     pub async fn add_conversation(&mut self, conversation: &str) -> Result<String, Box<dyn Error>> {
         let client = reqwest::Client::new();
-        let payload =json!(
+        let payload =
+            json!(
             {
                 "accountData": {
                 // devicePublicKey and  accountId should be changed to company values
@@ -214,15 +242,15 @@ impl<'a> BitteImageGenerator<'a> {
                 "message": conversation,
               }
         );
-        let response = client.post(format!("{}api/ai-router/v1/assistants",API_URL_BASE ))
+        let response = client
+            .post(format!("{}api/ai-router/v1/assistants", API_URL_BASE))
             .json(&payload)
-            .send()
-            .await?;
-
+            .send().await?;
 
         let response = response.text().await?;
-        if response.contains("proceed") || response.contains("suggested-prompts"){
-        let payload =json!(
+        if response.contains("proceed") || response.contains("suggested-prompts") {
+            let payload =
+                json!(
             {
                 "accountData": {
                 // devicePublicKey and  accountId should be changed to company values or creator values
@@ -251,28 +279,32 @@ impl<'a> BitteImageGenerator<'a> {
                 "message": "yes, mint for free",
               }
         );
-        client.post(format!("{}api/ai-router/v1/assistants",API_URL_BASE ))
-            .json(&payload)
-            .send()
-            .await?;
+            client
+                .post(format!("{}api/ai-router/v1/assistants", API_URL_BASE))
+                .json(&payload)
+                .send().await?;
         }
-        let response = client.get(format!("{}api/smart-action/create/{}",API_URL_BASE,self.session_key ))
-        .send()
-        .await.unwrap().json::<Response>().await.unwrap();
+        let response = client
+            .get(format!("{}api/smart-action/create/{}", API_URL_BASE, self.session_key))
+            .send().await
+            .unwrap()
+            .json::<Response>().await
+            .unwrap();
         // let response =from_str(response.as_str());
 
-    // Filter messages to find the one with `name: "generate-image"`
-     if let Some(message) = response.messages.iter().rev().find(|msg| {
-            if let Some(data) = &msg.data {
-                data.name == "generate-image"
-            } else {
-                false
-            }
-        }) {
+        // Filter messages to find the one with `name: "generate-image"`
+        if
+            let Some(message) = response.messages
+                .iter()
+                .rev()
+                .find(|msg| {
+                    if let Some(data) = &msg.data { data.name == "generate-image" } else { false }
+                })
+        {
             match &message.data {
-                None=> Err("Not Data".into()),
-                Some(data)=>{
-                    let _data= data.metadata.clone();
+                None => Err("Not Data".into()),
+                Some(data) => {
+                    let _data = data.metadata.clone();
                     if let Some(error) = _data.error {
                         if error != "null" {
                             return Err(format!("Error Generating image: {}", error).into());
@@ -281,13 +313,16 @@ impl<'a> BitteImageGenerator<'a> {
                     Ok(_data.url.unwrap())
                 }
             }
-        }else{
+        } else {
             Err("Not Found".into())
-        } 
+        }
     }
 
     pub async fn generate(&mut self, tweet_content: &str) -> Result<String, Box<dyn Error>> {
-        let prompt =format!("I need an NFT generated for this tweet (NB: the image must contain tweet data)  data:```{}```",tweet_content.to_string());
+        let prompt = format!(
+            "I need an NFT generated for this tweet (NB: the image must contain tweet data)  data:```{}```",
+            tweet_content.to_string()
+        );
         self.add_conversation(&prompt).await
     }
 }
@@ -300,8 +335,16 @@ mod test_bitte_image_generator {
     async fn test_async_image_generator() {
         let mut generator = BitteImageGenerator::new("xlassix.near").await.unwrap();
         assert_eq!(generator.session_key.len(), 21);
-        let image_url1=generator.generate("10015.io @10015io Hello world! 👋 Do you know that http://10015.io offers the best online tool for converting tweets into fancy images with lots of customization options? 🐦 ↪️ 🖼️ #tweet #image #converter").await.unwrap();
-        let image_url=generator.add_conversation("i need another image in abstract futuristic art style use the detail for NFT 1, Please don't forget the add the description into the image generated").await.unwrap();
-        assert_ne!(image_url,image_url1);
+        let image_url1 = generator
+            .generate(
+                "10015.io @10015io Hello world! 👋 Do you know that http://10015.io offers the best online tool for converting tweets into fancy images with lots of customization options? 🐦 ↪️ 🖼️ #tweet #image #converter"
+            ).await
+            .unwrap();
+        let image_url = generator
+            .add_conversation(
+                "i need another image in abstract futuristic art style use the detail for NFT 1, Please don't forget the add the description into the image generated"
+            ).await
+            .unwrap();
+        assert_ne!(image_url, image_url1);
     }
 }
