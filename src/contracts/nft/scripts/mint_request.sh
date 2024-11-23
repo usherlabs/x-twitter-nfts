2bf8320e348b78664f2d5cad2aea8dd64b9de213
xlassix <awhy14539@gmail.com>
2024-11-23 14:48:13 +0100

    containerized

--- src/contracts/nft/scripts/mint_request.sh
+++ src/contracts/nft/scripts/mint_request.sh
@@ -1,9 +1,9 @@
 # both accounts must be the same
 export NEAR_CONTRACT_ACCOUNT=local-nft.testnet
 export NEAR_SIGNER_ACCOUNT=local-verifier.testnet
-export TWEET_ID=1859567727305949191
+export TWEET_ID=1859829435182219408
 export IMAGE_URL="https://ipfs.io/ipfs/QmXLiZP95g8h71QTqzkPvQjKkc52cus4QnkYYbAmnekmRs"
-export NOTIFY="@ryan_sorry"
+export NOTIFY="@ryan_soury"
 
 
 near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '0.00587 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
--- start_notary_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# ? use this to start the notary server
-cd src/notary
-cargo run --release
-cd ..
