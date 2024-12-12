00f707e878b068c5c6ea769c452c2c851fd83401
xlassix <awhy14539@gmail.com>
2024-12-12 13:47:12 +0100

    Update dependencies versions and set new verifier address

--- src/contracts/verifier/contract/scripts/set_verifier.sh
+++ src/contracts/verifier/contract/scripts/set_verifier.sh
@@ -1,6 +1,6 @@
 # both accounts must be the same
 export NEAR_CONTRACT_ACCOUNT=local-verifier.testnet
 export NEAR_SIGNER_ACCOUNT=local-verifier.testnet
-export VERIFIER_ADDRESS="0x0fa46fc0560fad2d31d7183938c3ad68c4db1472"
+export VERIFIER_ADDRESS="0x373e547deddcf99bb6f11f6306230c560a663084"
 
 near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT set_verifier_address json-args '{"new_contract_address": "'$VERIFIER_ADDRESS'"}' prepaid-gas '100.0 Tgas' attached-deposit '0 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
