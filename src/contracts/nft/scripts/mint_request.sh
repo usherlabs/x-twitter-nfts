a4a7d5562a9f73c69e9d5dfec8afba98d9faa58a
xlassix <awhy14539@gmail.com>
2025-01-17 01:24:18 +0100

    Update mint_request.sh script with increased NEAR deposit

--- src/contracts/nft/scripts/mint_request.sh
+++ src/contracts/nft/scripts/mint_request.sh
@@ -6,4 +6,4 @@ export IMAGE_URL="https://ipfs.io/ipfs/QmXLiZP95g8h71QTqzkPvQjKkc52cus4QnkYYbAmn
 export NOTIFY="@ryan_soury"
 
 
-near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '0.00587 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
+near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '0.01587 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
