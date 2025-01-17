20b989fdfae09ab921f485b4ba14f28de62e2abc
xlassix <awhy14539@gmail.com>
2025-01-17 17:17:40 +0100

    Update NFT metadata provider and mint request parameters

--- src/contracts/nft/scripts/mint_request.sh
+++ src/contracts/nft/scripts/mint_request.sh
@@ -1,9 +1,9 @@
 # both accounts must be the same
 export NEAR_CONTRACT_ACCOUNT=x-bitte-nfts.testnet
 export NEAR_SIGNER_ACCOUNT=local-verifier.testnet
-export TWEET_ID=1859829435182219408
+export TWEET_ID=1879971246206857614
 export IMAGE_URL="https://ipfs.io/ipfs/QmXLiZP95g8h71QTqzkPvQjKkc52cus4QnkYYbAmnekmRs"
 export NOTIFY="@ryan_soury"
 
 
-near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '0.01587 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
+near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '1 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
