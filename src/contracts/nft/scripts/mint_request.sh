85f4120db863435a21f8f7e4d30de214a537a677
xlassix <awhy14539@gmail.com>
2025-04-10 09:56:46 +0100

    Refactor and add comments to scripts and lib.rs

--- src/contracts/nft/scripts/mint_request.sh
+++ src/contracts/nft/scripts/mint_request.sh
@@ -1,9 +1,9 @@
 # both accounts must be the same
 export NEAR_CONTRACT_ACCOUNT="x-bitte-nfts.testnet"
 export NEAR_SIGNER_ACCOUNT=local-verifier.testnet
-export TWEET_ID=1880053253305459117
+export TWEET_ID=1583095539742408706
 export IMAGE_URL="https://ipfs.io/ipfs/QmXLiZP95g8h71QTqzkPvQjKkc52cus4QnkYYbAmnekmRs"
-export NOTIFY="@xlassix"
+export NOTIFY="xlassix"
 
 
 near contract call-function as-transaction $NEAR_CONTRACT_ACCOUNT mint_tweet_request json-args '{"tweet_id": "'$TWEET_ID'","image_url":"'$IMAGE_URL'","notify":"'$NOTIFY'"}' prepaid-gas '100.0 Tgas' attached-deposit '10 NEAR' sign-as $NEAR_SIGNER_ACCOUNT network-config testnet sign-with-keychain send
--- src/contracts/verifier/contract/src/lib.rs
+++ src/contracts/verifier/contract/src/lib.rs
@@ -16,84 +16,95 @@ pub mod external;
 pub use crate::external::*;
 
 /// Selector for `isJournalVerified(bytes)`.
-/// The value is computing by taking the first 4 bytes of the keccak hash of the type
+/// The value is computed by taking the first 4 bytes of the keccak hash of the type
 /// signature for the function, see https://www.4byte.directory/signatures/?bytes4_signature=0xdb3e2198
 const IS_JOURNAL_VERIFIED_SELECTOR: [u8; 4] = [181, 76, 30, 108];
 const DEPOSIT: u128 = 15020000000000000000000;
 
-/// The tweet structure gotten from the API
+/// The tweet structure obtained from the API
 ///
-/// Containing the details about a tweet
+/// Contains the details about a tweet
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Tweet {
-    /// data
+    /// Data related to the tweet
     pub data: Vec<Data>,
-    /// users info
+    /// User information included with the tweet
     pub includes: Includes,
 }
 
 /// The data substructure of a tweet
 ///
-/// Containing the details about a tweet
+/// Contains the details about a tweet
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Data {
-    /// date created
+    /// Date the tweet was created
     pub created_at: String,
-    /// id
+    /// Unique identifier for the tweet
     pub id: String,
-    /// public_metrics
+    /// Public metrics associated with the tweet
     pub public_metrics: PublicMetrics,
-    /// edit_history_tweet_ids
+    /// History of tweet edits
     pub edit_history_tweet_ids: Vec<String>,
-    /// author_id of tweet
+    /// Author ID of the tweet
     pub author_id: String,
-    /// tweet text
+    /// Text content of the tweet
     pub text: String,
 }
 
 /// The PublicMetrics substructure of a tweet
 ///
-/// Containing the details about a tweet
+/// Contains the details about a tweet's public metrics
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PublicMetrics {
-    /// pub retweet_count
+    /// Number of retweets
     pub retweet_count: u32,
-    /// pub reply_count
+    /// Number of replies
     pub reply_count: u32,
-    /// pub like_count
+    /// Number of likes
     pub like_count: u32,
-    /// pub quote_count
+    /// Number of quotes
     pub quote_count: u32,
-    /// pub bookmark_count
+    /// Number of bookmarks
     pub bookmark_count: u32,
-    /// pub impression_count
+    /// Number of impressions
     pub impression_count: u32,
 }
 
 /// The Includes substructure of a metadata NFT
 ///
-/// Containing the details about a metadata NFT
+/// Contains the details about a metadata NFT
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Includes {
-    /// users
+    /// List of users associated with the tweet
     pub users: Vec<User>,
 }
 
 /// The User substructure of a tweet
 ///
-/// Containing the details about a tweet
+/// Contains the details about a user associated with a tweet
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
-    /// pub id: String,
+    /// Unique identifier for the user
     pub id: String,
-    /// pub username: Option<String>
+    /// Optional username of the user
     pub username: Option<String>, // Optional because it's not present in every user object
-    /// pub name: String,
+    /// Name of the user
     pub name: String,
-    /// pub created_at: String,
+    /// Date the user account was created
     pub created_at: String,
 }
 
+/// Generates a token metadata payload for a tweet NFT
+///
+/// # Arguments
+///
+/// * `json_tweet` - A JSON string representing the tweet
+/// * `image_url` - URL of the image associated with the tweet
+/// * `owner_account_id` - Account ID of the owner
+///
+/// # Returns
+///
+/// * `TokenMetadata` - The generated token metadata
 pub fn generate_tweet_nft_payload(
     json_tweet: &str,
     image_url: String,
@@ -101,15 +112,15 @@ pub fn generate_tweet_nft_payload(
 ) -> TokenMetadata {
     env::log_str(&format!("data:{}", json_tweet));
 
-    // get the tweet and the public metric to be stringified
+    // Deserialize the tweet JSON and extract the first tweet data and its public metrics
     let tweet: Tweet = serde_json::from_str(json_tweet).unwrap();
     let tweet_data = tweet.data.get(0).unwrap();
     let public_metric = &tweet_data.public_metrics;
 
-    // generate a token metadata
+    // Generate a token metadata object
     let token_metadata = TokenMetadata {
-        title: Some(tweet_data.id.clone()), // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
-        description: Some(tweet_data.text.clone()), // free-form description
+        title: Some(tweet_data.id.clone()), // Title of the NFT, using the tweet ID
+        description: Some(tweet_data.text.clone()), // Description of the NFT, using the tweet text
         extra: Some(
             json!({
                "public_metric": public_metric,
@@ -123,22 +134,30 @@ pub fn generate_tweet_nft_payload(
                 })).username
             })
             .to_string(),
-        ), // anything extra the NFT wants to store on-chain. Can be stringified JSON.
-        media: Some(image_url), // URL to associated media, preferably to decentralized, content-addressed storage
-        media_hash: None, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
-        copies: Some(1), // number of copies of this set of metadata in existence when token was minted.
+        ), // Additional data stored on-chain, can be stringified JSON
+        media: Some(image_url), // URL to associated media, preferably decentralized storage
+        media_hash: None, // Base64-encoded sha256 hash of content referenced by the `media` field
+        copies: Some(1), // Number of copies of this metadata in existence when token was minted
         issued_at: None, // ISO 8601 datetime when token was issued or minted
         expires_at: None, // ISO 8601 datetime when token expires
         starts_at: None, // ISO 8601 datetime when token starts being valid
         updated_at: None, // ISO 8601 datetime when token was last updated
-        reference: None, // URL to an off-chain JSON file with more info.
-        reference_hash: None,
+        reference: None, // URL to an off-chain JSON file with more info
+        reference_hash: None, // Base64-encoded sha256 hash of JSON referenced by the `reference` field
     };
 
     token_metadata
 }
 
 /// Converts a hexadecimal string (optionally prefixed with '0x') to a vector of bytes.
+///
+/// # Arguments
+///
+/// * `str` - A hexadecimal string
+///
+/// # Returns
+///
+/// * `Vec<u8>` - A vector of bytes representing the hexadecimal string
 pub fn string_to_vec_u8(str: &str) -> Vec<u8> {
     let starts_from: usize;
     if str.starts_with("0x") {
@@ -154,6 +173,14 @@ pub fn string_to_vec_u8(str: &str) -> Vec<u8> {
 }
 
 /// Hashes an Ethereum message to prepare it for public key derivation
+///
+/// # Arguments
+///
+/// * `message` - The message to be hashed
+///
+/// # Returns
+///
+/// * `Vec<u8>` - The hashed message
 fn hash_eth_message<T: AsRef<[u8]>>(message: T) -> Vec<u8> {
     const PREFIX: &str = "\x19Ethereum Signed Message:\n";
 
@@ -169,6 +196,15 @@ fn hash_eth_message<T: AsRef<[u8]>>(message: T) -> Vec<u8> {
     get_keccak256_hash(&eth_message).to_vec()
 }
 
+/// Computes the Keccak256 hash of a given message
+///
+/// # Arguments
+///
+/// * `eth_message` - The message to be hashed
+///
+/// # Returns
+///
+/// * `Vec<u8>` - The Keccak256 hash of the message
 fn get_keccak256_hash(eth_message: &[u8]) -> Vec<u8> {
     let hash = Keccak256::digest(eth_message);
     hash.to_vec()
@@ -177,17 +213,29 @@ fn get_keccak256_hash(eth_message: &[u8]) -> Vec<u8> {
 #[near_bindgen]
 #[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
 pub struct VerifierProxy {
-    aurora: AccountId,
-    nft_account_id: AccountId,
-    ic_remote_public_key: Address,
-    contract_address: Address,
+    aurora: AccountId, // Account ID of the Aurora contract
+    nft_account_id: AccountId, // Account ID of the NFT contract
+    ic_remote_public_key: Address, // Remote public key address
+    contract_address: Address, // Contract address
 }
 
 #[near_bindgen]
 impl VerifierProxy {
+    /// Initializes the VerifierProxy contract
+    ///
+    /// # Arguments
+    ///
+    /// * `aurora` - Account ID of the Aurora contract
+    /// * `nft_account_id` - Account ID of the NFT contract
+    /// * `ic_remote_public_key` - Remote public key as a string
+    /// * `contract_address` - Contract address as a string
+    ///
+    /// # Returns
+    ///
+    /// * `Self` - An instance of VerifierProxy
     #[init]
     pub fn init(
-        aurora: AccountId, //this can be safely set to "aurora"
+        aurora: AccountId, // This can be safely set to "aurora"
         nft_account_id: AccountId,
         ic_remote_public_key: String,
         contract_address: String,
@@ -203,6 +251,11 @@ impl VerifierProxy {
         }
     }
 
+    /// Retrieves the current state of the contract
+    ///
+    /// # Returns
+    ///
+    /// * `(AccountId, Address, Address)` - Tuple containing the NFT account ID, contract address, and remote public key
     pub fn get_contract_state(&self) -> (AccountId, Address, Address) {
         return (
             self.nft_account_id.clone(),
@@ -213,6 +266,14 @@ impl VerifierProxy {
 
     /// Generates a Merkle tree from a vector of ProofResponse objects.
     /// Each ProofResponse is hashed to create the leaves of the tree.
+    ///
+    /// # Arguments
+    ///
+    /// * `proof` - A string representing the proof
+    ///
+    /// # Returns
+    ///
+    /// * `String` - The Merkle root as a hexadecimal string
     pub fn generate_merkle_tree(&self, proof: String) -> String {
         // Convert each ProofResponse into a 32-byte hash to serve as a leaf in the Merkle tree.
         let leaves: Vec<[u8; 32]> = vec![proof]
@@ -229,6 +290,16 @@ impl VerifierProxy {
         hex::encode(merkle_root)
     }
 
+    /// Verifies an ECDSA signature against a given proof
+    ///
+    /// # Arguments
+    ///
+    /// * `proof` - A string representing the proof
+    /// * `signature` - A string representing the signature
+    ///
+    /// # Returns
+    ///
+    /// * `bool` - True if the verification is successful, false otherwise
     pub fn ecdsa_verification(&self, proof: String, signature: String) -> bool {
         let root_hash = hash_eth_message(self.generate_merkle_tree(proof));
         let signature = string_to_vec_u8(&signature);
@@ -245,6 +316,18 @@ impl VerifierProxy {
         add.encode().to_lowercase() == self.ic_remote_public_key.encode().to_lowercase()
     }
 
+    /// Verifies a proof and mints an NFT if the verification is successful
+    ///
+    /// # Arguments
+    ///
+    /// * `proof` - A string representing the proof
+    /// * `signature` - A string representing the signature
+    /// * `image_url` - URL of the image associated with the NFT
+    /// * `owner_address` - Account ID of the owner
+    ///
+    /// # Returns
+    ///
+    /// * `Promise` - A promise representing the NFT minting operation
     pub fn verify_proof_v2(
         &self,
         proof: String,
@@ -267,7 +350,7 @@ impl VerifierProxy {
                     minted_to: AccountId,
                 }
 
-                // mint the NFT here after a successfull verification
+                // Mint the NFT here after a successful verification
                 let token_id = token_metadata.title.clone().unwrap();
                 let receiver_id: MetadataExtra =
                     serde_json::from_str(&token_metadata.clone().extra.unwrap_or(
@@ -292,11 +375,20 @@ impl VerifierProxy {
         }
     }
 
-    //make this payable and provide the metadata
+    /// Verifies a proof and provides the metadata for NFT minting
+    ///
+    /// # Arguments
+    ///
+    /// * `journal` - A vector of bytes representing the journal
+    /// * `token_metadata` - Metadata of the token to be minted
+    ///
+    /// # Returns
+    ///
+    /// * `Promise` - A promise representing the NFT minting operation
     pub fn verify_proof(&self, journal: Vec<u8>, token_metadata: TokenMetadata) -> Promise {
         let journal_output = ethabi::Token::Bytes(journal.clone().into());
 
-        // assert that the journal is equal to the sha256 hash of the stringified token metadata
+        // Assert that the journal is equal to the sha256 hash of the stringified token metadata
         let stringified_token_metadata = serde_json::to_string(&token_metadata).unwrap();
         let stringified_token_metadata = stringified_token_metadata.as_bytes();
 
@@ -323,23 +415,47 @@ impl VerifierProxy {
             .then(Self::ext(env::current_account_id()).parse_verification_response(token_metadata))
     }
 
+    /// Sets a new contract address for the verifier
+    ///
+    /// # Arguments
+    ///
+    /// * `new_contract_address` - New contract address as a string
     #[private]
     pub fn set_verifier_address(&mut self, new_contract_address: String) {
         self.contract_address = aurora_sdk::parse_address(&new_contract_address).unwrap();
     }
 
+    /// Sets a new NFT account
+    ///
+    /// # Arguments
+    ///
+    /// * `nft_contract` - New NFT contract as a string
     #[private]
     pub fn set_nft_account(&mut self, nft_contract: String) {
         self.nft_account_id = nft_contract.parse().unwrap();
     }
 
+    /// Sets a new remote public key address
+    ///
+    /// # Arguments
+    ///
+    /// * `ic_public_address` - New remote public key address as a string
     #[private]
     pub fn set_ic_public_address(&mut self, ic_public_address: String) {
         self.ic_remote_public_key = aurora_sdk::parse_address(&ic_public_address).unwrap()
     }
 
-    // Callback used to parse the output from the call to Aurora made in `exact_output_single`.
-    // TODO pass in the nft payload to this function and mint the NFT after this callback
+    /// Callback used to parse the output from the call to Aurora made in `exact_output_single`.
+    /// TODO: Pass in the NFT payload to this function and mint the NFT after this callback
+    ///
+    /// # Arguments
+    ///
+    /// * `token_metadata` - Metadata of the token to be minted
+    /// * `result` - Result of the Aurora call
+    ///
+    /// # Returns
+    ///
+    /// * `Promise` - A promise representing the NFT minting operation
     #[private]
     pub fn parse_verification_response(
         &mut self,
@@ -354,7 +470,7 @@ impl VerifierProxy {
                 // Parse only the last bit and use that to determine if this is true or false
                 let is_valid = bytes.get(31).unwrap().clone() == 1;
 
-                // // if this proof is invalid then throw an error
+                // If this proof is invalid then throw an error
                 if !is_valid {
                     env::panic_str(&format!("invalid Payload: {:?}", bytes));
                 }
@@ -364,7 +480,7 @@ impl VerifierProxy {
                     minted_to: AccountId,
                 }
 
-                // mint the NFT here after a successfull verification
+                // Mint the NFT here after a successful verification
                 let token_id = token_metadata.title.clone().unwrap();
                 let receiver_id: MetadataExtra =
                     serde_json::from_str(&token_metadata.clone().extra.unwrap_or(
@@ -392,6 +508,15 @@ impl VerifierProxy {
         }
     }
 
+    /// Callback function to handle the result of the NFT creation
+    ///
+    /// # Arguments
+    ///
+    /// * `call_result` - Result of the NFT creation call
+    ///
+    /// # Returns
+    ///
+    /// * `bool` - True if the NFT creation was successful, false otherwise
     #[private]
     pub fn nft_creation_callback(
         &mut self,
